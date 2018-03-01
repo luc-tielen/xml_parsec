fe749e698e98f8bd33180139f1fdd836354f049c
Luc Tielen <luc.tielen@kabisa.nl>
2018-03-01 21:05:33 +0100

    Simplify types

--- lib/xml/tag.ex
+++ lib/xml/tag.ex
@@ -5,9 +5,9 @@ defmodule XML.Tag do
 
   @type t :: %__MODULE__{
     name: String.t(),
-    attributes: [XML.Attribute.t()],
-    contents: %__MODULE__{} | String.t() | nil
+    attributes: %{},
+    value: %__MODULE__{} | String.t() | nil
   }
 
-  defstruct [:name, :attributes, :contents]
+  defstruct [:name, :attributes, :value]
 end
