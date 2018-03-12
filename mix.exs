c48adea5b998488a67e03c0012edd0b11bec9b20
Luc Tielen <luc.tielen@kabisa.nl>
2018-03-12 08:43:28 +0100

    Fix version spec in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XmlParsec.MixProject do
     [
       app: :xml_parsec,
       version: "0.1.0",
-      elixir: ">= 1.4",
+      elixir: ">= 1.4.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
