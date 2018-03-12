45cf94764d1be5ec467b126c3da9adcac38969f6
Luc Tielen <luc.tielen@kabisa.nl>
2018-03-12 08:05:18 +0100

    Allow older Elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XmlParsec.MixProject do
     [
       app: :xml_parsec,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: ">= 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
